8e1a910de169b310b3b2e922122cd4b60df2b3ce
Meyer Kimera <meyer@umomail.com>
2020-01-30 16:38:32 -0500

    Imported and registered the product component into the products index.vue file.

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,22 +1,53 @@
 <template>
   <section class="products">
-    <article class="product">
-      <div class="thumbnail" style="background-image: url('https://shop.designsforhealth.com/assets/images/items/5htp-supreme_60%20capsules-1.png?resizeid=7&resizeh=500&resizew=500')"></div>
-      <h1>5-HTP SUPREME™</h1>
-      <p>5•HTP Supreme™ is a synergistic formula of 5-hydroxytryptophan (100 mg)</p>
-    </article>
-    <article class="product">
-      <div class="thumbnail" style="background-image: url('https://shop.designsforhealth.com/assets/images/items/5htp-synergy_90%20capsules-1.png?resizeid=7&resizeh=500&resizew=500')"></div>
-      <h1>5-HTP SYNERGY™</h1>
-      <p>5•HTP Synergy™ is a synergistic formula of 5-hydroxytryptophan and vitamin</p>
-    </article>
-    <article class="product">
-      <div class="thumbnail" style="background-image: url('https://shop.designsforhealth.com/assets/images/items/7-keto_60%20capsules-1.png?resizeid=7&resizeh=500&resizew=500')"></div>
-      <h1>7-KETO®</h1>
-      <p>7-Keto® is a registered trade name for the compound 3-acetyl-7-oxo DHEA</p>
-    </article>
+    <Product
+      thumbnail="https://shop.designsforhealth.com/assets/images/items/5htp-supreme_60%20capsules-1.png?resizeid=7&resizeh=500&resizew=500"
+      title="5-HTP SUPREME™"
+      preview-text="5•HTP Supreme™ is a synergistic formula of 5-hydroxytryptophan (100 mg)"
+    />
+    <Product
+      thumbnail="https://shop.designsforhealth.com/assets/images/items/5htp-synergy_90%20capsules-1.png?resizeid=7&resizeh=500&resizew=500"
+      title="5-HTP SYNERGY™"
+      preview-text="5•HTP Synergy™ is a synergistic formula of 5-hydroxytryptophan and vitamin"
+    />
+    <Product
+      thumbnail="https://shop.designsforhealth.com/assets/images/items/7-keto_60%20capsules-1.png?resizeid=7&resizeh=500&resizew=500"
+      title="7-KETO®"
+      preview-text="7-Keto® is a registered trade name for the compound 3-acetyl-7-oxo DHEA"
+    />
+
+<!-- Initial Products Page Instances Concept   -->
+<!--    <article class="product">-->
+<!--      <div class="thumbnail" style="background-image: url('https://shop.designsforhealth.com/assets/images/items/5htp-supreme_60%20capsules-1.png?resizeid=7&resizeh=500&resizew=500')"></div>-->
+<!--      <h1>5-HTP SUPREME™</h1>-->
+<!--      <p>5•HTP Supreme™ is a synergistic formula of 5-hydroxytryptophan (100 mg)</p>-->
+<!--    </article>-->
+<!--    <article class="product">-->
+<!--      <div class="thumbnail" style="background-image: url('https://shop.designsforhealth.com/assets/images/items/5htp-synergy_90%20capsules-1.png?resizeid=7&resizeh=500&resizew=500')"></div>-->
+<!--      <h1>5-HTP SYNERGY™</h1>-->
+<!--      <p>5•HTP Synergy™ is a synergistic formula of 5-hydroxytryptophan and vitamin</p>-->
+<!--    </article>-->
+<!--    <article class="product">-->
+<!--      <div class="thumbnail" style="background-image: url('https://shop.designsforhealth.com/assets/images/items/7-keto_60%20capsules-1.png?resizeid=7&resizeh=500&resizew=500')"></div>-->
+<!--      <h1>7-KETO®</h1>-->
+<!--      <p>7-Keto® is a registered trade name for the compound 3-acetyl-7-oxo DHEA</p>-->
+<!--    </article>-->
   </section>
 </template>
+
+<script>
+// Imported and registered the product component into the products index.vue file.
+// This gives you direct access to this component and also provides access to the products instances
+// import Product from '@/components/Recipe'
+import Product from '../../components/Product';
+
+export default {
+  components: {Product},
+  comments: {
+    Product
+  }
+}
+</script>
 
 <style scoped>
   .products {
