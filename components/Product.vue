09cf491830c79e55ed6913bc8f5f62aac1a77ae6
Meyer Kimera <meyer@umomail.com>
2020-01-30 16:39:53 -0500

    Style the product component

--- components/Product.vue
+++ components/Product.vue
@@ -13,4 +13,21 @@
   }
 </script>
 
+<style>
+  .product {
+    box-sizing: border-box;
+    width: 280px;
+    padding: 8px;
+    border: 1px solid #ccc;
+    box-shadow: 0 2px 2px #aaa;
+    margin: 10px;
+  }
+  .thumbnail {
+    background-position: center;
+    background-size: cover;
+    width: 100%;
+    height: 200px;
+  }
+</style>
+
 
