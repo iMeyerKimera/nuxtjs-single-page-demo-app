b6a84c5e59bcab0f9e21fe230ca02ee4b2e5cf8d
Meyer Kimera <meyer@umomail.com>
2020-01-30 15:59:44 -0500

    add product component

---
+++ components/Product.vue
@@ -0,0 +1,13 @@
+<template>
+  <article class="product">
+    <div class="thumbnail" style="background-image: url('https://shop.designsforhealth.com/assets/images/items/5htp-supreme_60%20capsules-1.png?resizeid=7&resizeh=500&resizew=500')"></div>
+    <h1>5-HTP SUPREME™</h1>
+    <p>5•HTP Supreme™ is a synergistic formula of 5-hydroxytryptophan (100 mg)</p>
+  </article>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
