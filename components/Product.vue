354d95737bc1964a23dc546e0ed1a2deab7626c5
Meyer Kimera <meyer@umomail.com>
2020-01-30 16:09:05 -0500

    finished building the product component

--- components/Product.vue
+++ components/Product.vue
@@ -1,13 +1,16 @@
 <template>
   <article class="product">
-    <div class="thumbnail" style="background-image: url('https://shop.designsforhealth.com/assets/images/items/5htp-supreme_60%20capsules-1.png?resizeid=7&resizeh=500&resizew=500')"></div>
-    <h1>5-HTP SUPREME™</h1>
-    <p>5•HTP Supreme™ is a synergistic formula of 5-hydroxytryptophan (100 mg)</p>
+    <!-- bind style for background image to a dynamic value-->
+    <div class="thumbnail" :style="{backgroundImage: 'url(' + thumbnail + ')'}"></div>
+    <h1>{{title}}</h1>
+    <p>{{previewText}}</p>
   </article>
 </template>
 
 <script>
   export default {
-
+    props: ['thumbnail', 'title', 'previewText']
   }
 </script>
+
+
