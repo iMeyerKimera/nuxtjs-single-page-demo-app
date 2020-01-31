4447e89dfe4845158f40c641fcc71cf7bde5d72c
Meyer Kimera <meyer@umomail.com>
2020-01-30 19:54:09 -0500

    Added product  asyncData method to product detail view, Render product detail dynamically, styled product page "(-id/index.vue)". Bind product instances to asyncData method "(products/index.vue)"

--- pages/products/_id/index.vue
+++ pages/products/_id/index.vue
@@ -1,10 +1,17 @@
 <template>
   <section class="product-view">
-    <h1>{{$route.params.id}}</h1>
+    <h1>{{product.title}}</h1>
     <div>
-      <img src="" alt="">
+      <img class="product-media" :src="product.thumbnail" :alt="product.title">
     </div>
-    <p>The product description.</p>
+    <p>{{product.descriptionText}}</p>
+
+<!-- Initial object properties-->
+<!--    <h1>{{$route.params.id}}</h1>-->
+<!--    <div>-->
+<!--      <img src="" alt="">-->
+<!--    </div>-->
+<!--    <p>The product description.</p>-->
   </section>
 </template>
 
@@ -16,5 +23,63 @@
     align-items: center;
     text-align: center;
     padding: 30px;
+  }
+</style>
+
+<script>
+  export default {
+  asyncData(context) {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve({
+            product: [
+              {
+                id: "1",
+                title: "5-HTP SUPREME™",
+                previewText: "5•HTP Supreme™ is a synergistic formula of 5-hydroxytryptophan (100 mg)",
+                descriptionText: "5•HTP Supreme™ is a synergistic formula of 5-hydroxytryptophan (100 mg) and vitamin B6 (20 mg) for supporting overall neurotransmitter metabolism by providing precursors of serotonin. 5-HTP readily enters the blood brain barrier, however conversion into serotonin requires the cofactor vitamin B6, which is included in this formula. 5•HTP Supreme™ may be used during the day to support healthy mood or appetite, or at bedtime to support sleep.**\n" +
+                  "\n" +
+                  "\n" +
+                  "This formula can be used in place of 5•HTP Synergy™ when higher doses of 5-HTP are recommended.",
+                thumbnail: "https://shop.designsforhealth.com/assets/images/items/5htp-supreme_60%20capsules-1.png?resizeid=7&resizeh=500&resizew=500"
+              },
+              {
+                id: "2",
+                title: "5-HTP SYNERGY™",
+                previewText: "5•HTP Synergy™ is a synergistic formula of 5-hydroxytryptophan and vitamin",
+                descriptionText: "5•HTP Synergy™ is a synergistic formula of 5-hydroxytryptophan and vitamin B6 for supporting overall neurotransmitter metabolism by providing precursors of serotonin. 5-HTP readily enters the blood brain barrier; however, conversion into serotonin requires the cofactor vitamin B6, which is included in this formula. 5•HTP Synergy™ may be used during the day to support healthy mood or appetite, or at bedtime to support sleep.**",
+                thumbnail: "https://shop.designsforhealth.com/assets/images/items/5htp-synergy_90%20capsules-1.png?resizeid=7&resizeh=500&resizew=500"
+              },
+              {
+                id: "3",
+                title: "7-KETO®",
+                previewText: "7-Keto® is a registered trade name for the compound 3-acetyl-7-oxo DHEA",
+                descriptionText: "7-Keto® is a registered trade name for the compound 3-acetyl-7-oxo DHEA, a stabilized form of 7-oxo DHEA. Once ingested, acetyl-7-oxo DHEA is cleaved to 7-oxo DHEA, a DHEA metabolite naturally found in the body.\n" +
+                  "\n" +
+                  "\n" +
+                  "Supplementation with 7-Keto® may counteract the decline in 7-oxo DHEA levels that occurs during the normal aging process. Although 7-oxo DHEA is similar to DHEA, it does not convert into estrogenic or androgenic hormones.\n" +
+                  "\n",
+                thumbnail: "https://shop.designsforhealth.com/assets/images/items/7-keto_60%20capsules-1.png?resizeid=7&resizeh=500&resizew=500"
+              }
+            ].find(el => el.id === context.params.id)  // This executes to each element in the product objects and returns the right one
+            // with the object whose context is the element id.
+          })
+        }, 1500)
+      })
+    },
+  }
+</script>
+
+<style scoped>
+  .product-view {
+    display: flex;
+    flex-flow: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    padding: 30px;
+  }
+  .product-media {
+    width: 100%;
   }
 </style>
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,23 +1,34 @@
 <template>
   <section class="products">
+    <!-- Bind to a product object -->
     <Product
-      thumbnail="https://shop.designsforhealth.com/assets/images/items/5htp-supreme_60%20capsules-1.png?resizeid=7&resizeh=500&resizew=500"
-      title="5-HTP SUPREME™"
-      preview-text="5•HTP Supreme™ is a synergistic formula of 5-hydroxytryptophan (100 mg)"
-      id="1"
+      v-for="product in products"
+      :key="product.id"
+      :thumbnail="product.thumbnail"
+      :title="product.title"
+      :preview-text="product.previewText"
+      :id="product.id"
     />
-    <Product
-      thumbnail="https://shop.designsforhealth.com/assets/images/items/5htp-synergy_90%20capsules-1.png?resizeid=7&resizeh=500&resizew=500"
-      title="5-HTP SYNERGY™"
-      preview-text="5•HTP Synergy™ is a synergistic formula of 5-hydroxytryptophan and vitamin"
-      id="2"
-    />
-    <Product
-      thumbnail="https://shop.designsforhealth.com/assets/images/items/7-keto_60%20capsules-1.png?resizeid=7&resizeh=500&resizew=500"
-      title="7-KETO®"
-      preview-text="7-Keto® is a registered trade name for the compound 3-acetyl-7-oxo DHEA"
-      id="3"
-    />
+
+<!--  Second Products page instances concept  -->
+<!--    <Product-->
+<!--      thumbnail="https://shop.designsforhealth.com/assets/images/items/5htp-supreme_60%20capsules-1.png?resizeid=7&resizeh=500&resizew=500"-->
+<!--      title="5-HTP SUPREME™"-->
+<!--      preview-text="5•HTP Supreme™ is a synergistic formula of 5-hydroxytryptophan (100 mg)"-->
+<!--      id="1"-->
+<!--    />-->
+<!--    <Product-->
+<!--      thumbnail="https://shop.designsforhealth.com/assets/images/items/5htp-synergy_90%20capsules-1.png?resizeid=7&resizeh=500&resizew=500"-->
+<!--      title="5-HTP SYNERGY™"-->
+<!--      preview-text="5•HTP Synergy™ is a synergistic formula of 5-hydroxytryptophan and vitamin"-->
+<!--      id="2"-->
+<!--    />-->
+<!--    <Product-->
+<!--      thumbnail="https://shop.designsforhealth.com/assets/images/items/7-keto_60%20capsules-1.png?resizeid=7&resizeh=500&resizew=500"-->
+<!--      title="7-KETO®"-->
+<!--      previewText="7-Keto® is a registered trade name for the compound 3-acetyl-7-oxo DHEA"-->
+<!--      id="3"-->
+<!--    />-->
 
 <!-- Initial Products Page Instances Concept   -->
 <!--    <article class="product">-->
@@ -45,9 +56,46 @@
 import Product from '../../components/Product';
 
 export default {
-  components: {Product},
-  comments: {
+  components: {
     Product
+  },
+  // Simulate serving data from a server
+  // This asyncData is a special method executes on the server whenever request is made
+  // When you are on a single page mode and clicking around, it executes on the client, but if you send
+  // a request initially, it executes on the server to fetch all the data that needs to be rendered on the page
+  // in advance on the server, before returning the page so you can pre-render the page with the data
+  // on the server, and then send back the finished page to the user
+  //
+  asyncData() {
+    // return axios.get()
+    // Let's perform a face request to resolve data with asyncData but in reality, you would want to use
+    // some method such as axios.get()
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          products: [
+            {
+              id: "1",
+              title: "5-HTP SUPREME™",
+              previewText: "5•HTP Supreme™ is a synergistic formula of 5-hydroxytryptophan (100 mg)",
+              thumbnail: "https://shop.designsforhealth.com/assets/images/items/5htp-supreme_60%20capsules-1.png?resizeid=7&resizeh=500&resizew=500"
+            },
+            {
+              id: "2",
+              title: "5-HTP SYNERGY™",
+              previewText: "5•HTP Synergy™ is a synergistic formula of 5-hydroxytryptophan and vitamin",
+              thumbnail: "https://shop.designsforhealth.com/assets/images/items/5htp-synergy_90%20capsules-1.png?resizeid=7&resizeh=500&resizew=500"
+            },
+            {
+              id: "3",
+              title: "7-KETO®",
+              previewText: "7-Keto® is a registered trade name for the compound 3-acetyl-7-oxo DHEA",
+              thumbnail: "https://shop.designsforhealth.com/assets/images/items/7-keto_60%20capsules-1.png?resizeid=7&resizeh=500&resizew=500"
+            }
+          ]
+        })
+      }, 1500)
+    })
   },
 
 }
